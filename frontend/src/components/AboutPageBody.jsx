30802d65b2643a37020fce9d946ef7e646e5f0a2
lkaldyne <l.kaldyne@gmail.com>
2020-08-30 03:30:32 -0400

    setup pagebody template and added some page-specific css

---
+++ frontend/src/components/AboutPageBody.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Logo } from './Logo';
+import './styles/AboutPageBody.css'
+
+export class AboutPageBody extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="aboutpage_div">
+            </div>
+        );
+    }
+}

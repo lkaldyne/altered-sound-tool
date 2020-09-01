ee965a26e4be680f32b205f62c8579a55d8308e5
lkaldyne <l.kaldyne@gmail.com>
2020-09-01 14:55:48 -0400

    Started toolmodify page, further research needed on sliders

--- frontend/src/components/tool/ToolModify.jsx
+++ frontend/src/components/tool/ToolModify.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import './styles/ToolModify.css'
 
 export class ToolModify extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+
+        }
     }
 
     render() {
         return (
-            <div></div>
+            <div className="toolmodify_maindiv">
+                <div className="toolmodify_header">
+                    <div className="toolmodify_title">
+                        <p className="toolmodify_titletext">Your Audio File:</p>
+                    </div>
+                    <div className="toolmodify_title">
+                        <p className="toolmodify_filenametext">example1.wav</p>
+                    </div>
+                </div>
+                <div className="toolmodify_modbox">
+                    <div className="toolmodify_settingwrapper">
+                        <div className="toolmodify_settingtitle">
+                            <p className="toolmodify_settingtitletext">Audio Speed:</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }

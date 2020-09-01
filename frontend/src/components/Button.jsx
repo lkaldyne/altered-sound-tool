64bab9f97c24d33a973cbe9d046f3c080127fe20
lkaldyne <l.kaldyne@gmail.com>
2020-09-01 02:36:11 -0400

    added loading and disabled states to button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import Loader from 'react-loader-spinner'
 import './styles/Button.css'
 
 export function Button(props) {
     return (
         <div
             style={{ width: props.width, height: props.height }}
-            className="buttongeneral"
-            onClick={() => props.clicked(props.clickedarg)}
+            className={props.disabled ? "buttondisabled" : props.loading ? "buttonloading" : "buttongeneral"}
+            onClick={() => {
+                if (!props.disabled) {
+                    if (props.clickedarg) {
+                        props.clicked(props.clickedarg);
+                    }
+                    else {
+                        props.clicked();
+                    }
+                }
+            }}
         >
-            {props.children}
+            {
+                props.loading ?
+                    <Loader
+                        type="ThreeDots"
+                        color="black"
+                        height={50}
+                        width={40}
+                    />
+                    :
+                    props.children
+            }
         </div>
     );
 }

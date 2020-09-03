6f59a766f022065ed2b31194db5fdb664ecf927b
lkaldyne <l.kaldyne@gmail.com>
2020-09-03 02:28:51 -0400

    Added more customizability to button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,30 +5,40 @@ import './styles/Button.css'
 export function Button(props) {
     return (
         <div
-            style={{ width: props.width, height: props.height }}
-            className={props.disabled ? "buttondisabled" : props.loading ? "buttonloading" : "buttongeneral"}
-            onClick={() => {
-                if (!props.disabled) {
-                    if (props.clickedarg) {
-                        props.clicked(props.clickedarg);
-                    }
-                    else {
-                        props.clicked();
-                    }
-                }
+            style={{
+                width: props.width,
+                height: props.height,
+                background: props.bckg ? props.bckg : "rgba(0,0,0,0)",
+                margin: props.margin ? props.margin : "0 0 0 0",
+                color: props.textcolor ? props.textcolor : "white"
             }}
+            className="buttonwrapper"
         >
-            {
-                props.loading ?
-                    <Loader
-                        type="ThreeDots"
-                        color="black"
-                        height={50}
-                        width={40}
-                    />
-                    :
-                    props.children
-            }
+            <div
+                className={props.disabled ? "buttondisabled" : props.loading ? "buttonloading" : "buttongeneral"}
+                onClick={() => {
+                    if (!props.disabled) {
+                        if (props.clickedarg) {
+                            props.clicked(props.clickedarg);
+                        }
+                        else {
+                            props.clicked();
+                        }
+                    }
+                }}
+            >
+                {
+                    props.loading ?
+                        <Loader
+                            type="ThreeDots"
+                            color="black"
+                            height={50}
+                            width={40}
+                        />
+                        :
+                        props.children
+                }
+            </div>
         </div>
     );
 }
